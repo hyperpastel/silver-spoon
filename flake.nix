402cfd221f69e9e4c76ca68f0443df9a5d392801
hyperpastel <vmutze04@gmail.com>
2025-10-23 20:07:53 +0200

    feat: add mkdaily script

--- flake.nix
+++ flake.nix
@@ -29,6 +29,22 @@
             ${pkgs.runtimeShell} -c "cd $path && $EDITOR cf.cpp"
           '';
         })
+
+        (pkgs.writeShellApplication {
+          name = "mkdaily";
+          text = ''
+            name="$(date +%Y-%m-%d)"
+            typst init @local/flow "$name"
+
+            sed -i -e s/preview/local/ ./"$name"/main.typ
+
+            if [[ $# -gt 0 ]]; then
+                sed -i -e s/"Super cool title!"/"$1"/ ./"$name"/main.typ
+            fi
+
+            cd "$name" && $EDITOR main.typ
+          '';
+        })
       ];
     in
     {
